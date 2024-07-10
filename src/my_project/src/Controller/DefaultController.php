de779abba46faf08a3f3f1cf810ecc9299b304af
GalaninPI <galaninpi@ad.atlant.by>
2024-07-10 09:58:36 +0300

    main: example routes added

--- src/my_project/src/Controller/DefaultController.php
+++ src/my_project/src/Controller/DefaultController.php
@@ -15,4 +15,20 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
+    #[Route('/default/{id}', name: 'app_default_id', requirements: ['id' => '\d+'])]
+    public function index_id(int $id): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    #[Route('/default/q/{id}', name: 'app_default_id_POST', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function index_id_POST(int $id): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
 }
